8e798afaa69b054bd28c8f542351401f783fc020
Matthew Seo <rongjuni@hotmail.com>
2022-08-09 10:24:00 -0400

    javascript operator practice

--- app.js
+++ app.js
@@ -8,12 +8,60 @@ console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
 
+let a = 20;
+console.log("a", a);
+
+let b = 4;
+console.log("b", b);
+
+let add = a + b;
+console.log("a+b", add);
+
+let minus = a - b;
+console.log("a-b", minus);
+
+let multiply = a * b;
+console.log("a*b", multiply);
+
+let dividing = a / b;
+console.log("a/b", dividing);
+
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log("num + str", num + str);
+console.log("num + str2", num + str2);
+console.log("num + isPresent", num + isPresent);
+console.log("firstName + num", firstName + num);
+console.log("isPresent + str", isPresent + str);
+console.log("firstName + lastName", firstName + lastName);
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresentt = false;
+
+console.log("val == str3", val == str3);
+console.log("val === str3", val === str3);
+console.log("!isPresentt", !isPresentt);
+console.log(
+  '("eleven" == str4 && val >= str3)',
+  "eleven" == str4 && val >= str3
+);
+console.log("(!isPresentt || isPresentt", !isPresentt || isPresentt);
+console.log("0 == false", 0 == false);
+console.log("0 === false", 0 === false);
+console.log("0 != false", 0 != false);
+console.log("0 !== false", 0 !== false);
